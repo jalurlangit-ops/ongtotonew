9e5a338f1547c6dd6aaf54f46803006005a86b85
jalurlangit-ops <lxgroup.kece@gmail.com>
2025-01-19 19:59:55 +0700

    first commit

---
+++ app/var-global.js
@@ -0,0 +1,11 @@
+module.exports = {
+    title: "Title",
+    desc: "Desc",
+    keywords: "Key",
+    brandName: "Brand",
+    domain: "https://cccccc.com",
+    histats: '1,4920979,4,1,120,40,00012222',
+    gsc: 'aaabbbccccccc',
+    daftar: 'https://daftarwap.orang-dalam.link/lanaya1',
+    login: 'https://daftarwap.orang-dalam.link/lanaya2',
+}
