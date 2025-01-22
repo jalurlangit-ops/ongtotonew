f6ca69b1c59f1d36023049cbf0c1ef0aefa7e128
jalurlangit-ops <lxgroup.kece@gmail.com>
2025-01-22 21:17:47 +0700

    kw

--- app/var-global.js
+++ app/var-global.js
@@ -1,7 +1,7 @@
 module.exports = {
-    title: "Title",
-    desc: "Desc",
-    keywords: "Key",
+    title: "Ongtoto : Situs Gacor New Member 100 Dan Slot Bonus New Member 100 Persen Di Awal",
+    desc: "Ongtoto adalah situs slot gacor yang memberikan pengalaman bermain terbaik bagi para pemain baru. Dengan promo New Member 100, setiap pemain yang baru mendaftar dapat langsung menikmati bonus 100% pada deposit pertama mereka",
+    keywords: "Ongtoto, Situs Gacor New Member 100, Slot Bonus New Member 100 Persen Di Awal, Slot Paling Gacor 2025, Daftar Link Slot Gacor, Situs Togel Cashback Terbesar, Togel Terlengkap Dan Terpercaya, Akun Togel Resmi Di 2025, Link268 Slot Login",
     brandName: "Ongtoto",
     domain: "https://dbqj80.com",
     histats: '1,4923757,4,5,172,25,00010000',
