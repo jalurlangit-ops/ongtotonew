e3a2b00d00d7ad2c79eab188ed366905b6c5fb04
jalurlangit-ops <lxgroup.kece@gmail.com>
2025-01-25 20:11:26 +0700

    rating

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ function rank() {
   "aggregateRating": {
     "@type": "AggregateRating",
     "ratingValue": "5",
-    "ratingCount": "10394859"
+    "ratingCount": "10394854"
   },
 "review": {
         "@type": "Review",
