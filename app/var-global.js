7f298a384ebf44df5c67e7431a2e3f6bab7af5ab
jalurlangit-ops <lxgroup.kece@gmail.com>
2025-03-01 01:28:06 +0700

    daftar web

--- app/var-global.js
+++ app/var-global.js
@@ -6,6 +6,6 @@ module.exports = {
     domain: "https://dbqj80.com",
     histats: '1,4923757,4,5,172,25,00010000',
     gsc: 'hBBm9ERCb6JXtdmhks5sDJB5BQPN5jYA0Mw6CEykR1E',
-    daftar: 'https://daftarwap.orang-dalam.link/ong',
+    daftar: 'https://daftarweb.orang-dalam.link/ong',
     login: 'https://daftarwap.orang-dalam.link/ong',
 }
